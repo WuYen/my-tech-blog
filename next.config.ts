669faacb544bd9b815f1ef65bffc24c4aca4869a
“vince” <vince-yp.wu@nanshan.com.tw>
2025-01-08 14:11:02 +0800

    add out folder to remote

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/my-blog',
+  basePath: '/my-tech-blog',
 };
 
 export default nextConfig;
